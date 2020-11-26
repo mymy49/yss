048645d7d5377ea30b6e2e2a2207b1e2b4fe65b9
Yoon-Ki Hong <mymy49@nate.com>
2020-11-26 18:18:01 +0900

    파일의 스페이싱 조정

--- yss/drv/adc/drv_adc_st_type_C.cpp
+++ yss/drv/adc/drv_adc_st_type_C.cpp
@@ -62,82 +62,82 @@ namespace drv
 	Adc::Adc(ADC_TypeDef *peri, void (*clockFunc)(bool en), void (*nvicFunc)(bool en)) : Drv(clockFunc, nvicFunc)
 	{
 		mPeri = peri;
-        mIndex = 0;
-        mNumOfCh = 0;
+		mIndex = 0;
+		mNumOfCh = 0;
 
-        for(int i=0;i<18;i++)
-        {
+		for(int i=0;i<18;i++)
+		{
 			mChannel[i] = 0;
 			mResult[i] = 0;
-            mLpfLv[i] = define::adc::lpfLv::LV9;
+			mLpfLv[i] = define::adc::lpfLv::LV9;
 			mBit[i] = define::adc::bit::BIT16;
-        }
+		}
 	}
 
 	bool Adc::init(void)
-    {
+	{
 		// ADC on
-        mPeri->CR &= ~ADC_CR_DEEPPWD_Msk;
-        mPeri->CR |= ADC_CR_ADVREGEN_Msk;
-        mPeri->ISR = ADC_ISR_ADRDY_Msk;
+		mPeri->CR &= ~ADC_CR_DEEPPWD_Msk;
+		mPeri->CR |= ADC_CR_ADVREGEN_Msk;
+		mPeri->ISR = ADC_ISR_ADRDY_Msk;
 		mPeri->CR |= ADC_CR_ADEN_Msk;
-        while((mPeri->ISR & ADC_ISR_ADRDY_Msk) == 0)
+		while((mPeri->ISR & ADC_ISR_ADRDY_Msk) == 0)
 			thread::yield();
-        mPeri->ISR = ADC_ISR_ADRDY_Msk;
+		mPeri->ISR = ADC_ISR_ADRDY_Msk;
 
-        // 샘플 타임 설정
-        mPeri->SMPR1 =	ADC_SMPR1_SMP0_Msk | ADC_SMPR1_SMP1_Msk | ADC_SMPR1_SMP2_Msk | \
+		// 샘플 타임 설정
+		mPeri->SMPR1 =	ADC_SMPR1_SMP0_Msk | ADC_SMPR1_SMP1_Msk | ADC_SMPR1_SMP2_Msk | \
 						ADC_SMPR1_SMP3_Msk | ADC_SMPR1_SMP4_Msk | ADC_SMPR1_SMP5_Msk | \
-                        ADC_SMPR1_SMP6_Msk | ADC_SMPR1_SMP7_Msk | ADC_SMPR1_SMP8_Msk | \
-                        ADC_SMPR1_SMP9_Msk;
+						ADC_SMPR1_SMP6_Msk | ADC_SMPR1_SMP7_Msk | ADC_SMPR1_SMP8_Msk | \
+						ADC_SMPR1_SMP9_Msk;
 
-        mPeri->SMPR2 =	ADC_SMPR2_SMP10_Msk | ADC_SMPR2_SMP11_Msk | ADC_SMPR2_SMP12_Msk | \
+		mPeri->SMPR2 =	ADC_SMPR2_SMP10_Msk | ADC_SMPR2_SMP11_Msk | ADC_SMPR2_SMP12_Msk | \
 						ADC_SMPR2_SMP13_Msk | ADC_SMPR2_SMP14_Msk | ADC_SMPR2_SMP15_Msk | \
-                        ADC_SMPR2_SMP16_Msk | ADC_SMPR2_SMP17_Msk | ADC_SMPR2_SMP18_Msk;
+						ADC_SMPR2_SMP16_Msk | ADC_SMPR2_SMP17_Msk | ADC_SMPR2_SMP18_Msk;
 
-        mPeri->IER |= ADC_IER_EOCIE_Msk;
-        mPeri->CR |= ADC_CR_ADSTART_Msk;
-        return true;
-    }
+		mPeri->IER |= ADC_IER_EOCIE_Msk;
+		mPeri->CR |= ADC_CR_ADSTART_Msk;
+		return true;
+	}
 
 	void Adc::isr(void)
 	{
 		signed int dr = mPeri->DR << 19, temp, abs;
 		unsigned char index = mChannel[mIndex];
 
-        temp = dr - mResult[index];
+		temp = dr - mResult[index];
 		temp >>= mLpfLv[mIndex];
-        mResult[index] += temp;
-		
-        mIndex++;
-        if(mIndex >= mNumOfCh)
+		mResult[index] += temp;
+	
+		mIndex++;
+		if(mIndex >= mNumOfCh)
 			mIndex = 0;
-		
+	
 		mPeri->SQR1 &= ~ADC_SQR1_SQ1_Msk;
 		mPeri->SQR1 |= mChannel[mIndex] << ADC_SQR1_SQ1_Pos;
 		mPeri->CR |= ADC_CR_ADSTART_Msk;
 	}
 
 	void Adc::add(unsigned char pin, unsigned char lpfLv, unsigned char bit)
-    {
+	{
 		if(mNumOfCh >= 18)
 			return;
 		mChannel[mNumOfCh] = pin;
-        mLpfLv[mNumOfCh] = lpfLv;
-        mBit[pin] = bit;
+		mLpfLv[mNumOfCh] = lpfLv;
+		mBit[pin] = bit;
 		mNumOfCh++;
-    }
+	}
 
-    unsigned short Adc::get(unsigned char pin)
-    {
+	unsigned short Adc::get(unsigned char pin)
+	{
 		return mResult[pin] >> mBit[pin];
-    }
+	}
 }
 
 extern "C"
 {
 	void ADC1_2_IRQHandler(void)
-    {
+	{
 #if defined(ADC1_ENABLE) && defined(ADC1)
 		if(ADC1->IER & ADC_IER_EOCIE_Msk && ADC1->ISR & ADC_ISR_EOC_Msk)
 		{
@@ -152,7 +152,7 @@ extern "C"
 			adc2.isr();
 		}
 #endif
-    }
+	}
 }
 
 
