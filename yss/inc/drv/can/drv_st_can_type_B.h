6e715dde90a3a0c2d31920c9d70af4d8bb67e48c
Yoon-Ki Hong <mymy49@nate.com>
2021-01-11 13:28:38 +0900

    #00001 버그 수정
    https://cafe.naver.com/yssoperatingsystem/333

--- yss/inc/drv/can/drv_st_can_type_B.h
+++ yss/inc/drv/can/drv_st_can_type_B.h
@@ -13,7 +13,7 @@
 //
 //	Home Page : http://cafe.naver.com/yssoperatingsystem
 //	Copyright 2020.	yss Embedded Operating System all right reserved.
-//  
+//
 //  주담당자 : 아이구 (mymy49@nate.com) 2016.04.30 ~ 현재
 //  부담당자 : -
 //
@@ -22,58 +22,63 @@
 #ifndef YSS_DRV_CAN_ST_TYPE_B__H_
 #define YSS_DRV_CAN_ST_TYPE_B__H_
 
-#if defined (STM32G431xx) || defined (STM32G441xx) || \
-	defined (STM32G471xx) || defined (STM32G473xx) || defined (STM32G474xx) || defined (STM32G483xx) || defined (STM32G484xx) || defined (STM32GBK1CB)
+#if defined(STM32G431xx) || defined(STM32G441xx) || \
+    defined(STM32G471xx) || defined(STM32G473xx) || defined(STM32G474xx) || defined(STM32G483xx) || defined(STM32G484xx) || defined(STM32GBK1CB)
 
 #include "drv_st_can_type_A_define.h"
-#include <yss/thread.h>
 #include <drv/Drv.h>
+#include <yss/thread.h>
 
 namespace drv
 {
-	class Can : public Drv
-	{
-		struct RxFifo
-		{
-			unsigned esi : 1;
-			unsigned xtd : 1;
-			unsigned rtr : 1;
-			unsigned id : 29;
-        };
+class Can : public Drv
+{
+    struct RxFifo
+    {
+        unsigned esi : 1;
+        unsigned xtd : 1;
+        unsigned rtr : 1;
+        unsigned id : 29;
+    };
 
-		unsigned int *mData;
-		unsigned int mHead, mTail, mMaxDepth;
-		unsigned int (*mGetClockFreq)(void);
-		
-		unsigned int *mCanTxBuffer;
-        unsigned int *mCanTxEventFifo;
-        unsigned int *mCanRxFifo1;
-        unsigned int *mCanRxFifo0;
-        unsigned int *mCanExtFilter;
-        unsigned int *mCanStdFilter;
+    unsigned int *mData;
+    unsigned int mHead, mTail, mMaxDepth;
+    unsigned int (*mGetClockFreq)(void);
 
-        unsigned char mTxFifoIndex;
+    unsigned int *mCanTxBuffer;
+    unsigned int *mCanTxEventFifo;
+    unsigned int *mCanRxFifo1;
+    unsigned int *mCanRxFifo0;
+    unsigned int *mCanExtFilter;
+    unsigned int *mCanStdFilter;
 
-		FDCAN_GlobalTypeDef *mPeri;
-		Mutex mMutex;
+    unsigned char mTxFifoIndex;
 
-	public :
-		Can(FDCAN_GlobalTypeDef *peri, void (*clockFunc)(bool en), void (*nvicFunc)(bool en), unsigned int (*getClockFreq)(void));
-		bool init(unsigned int baudRate, unsigned int bufDepth, float samplePoint = 0.875);
-		void push(unsigned int *data);
-        bool isReceived(void);
-		bool isStandard(void);
-		unsigned int getIdentifier(void);
-		unsigned char getPriority(void);
-		unsigned short getPgn(void);
-        unsigned char getSrcAddr(void);
-		unsigned char getSize(void);
-		char* getData(void);
-        void flush(void);
-		void releaseFifo(void);
-		bool send(unsigned char priority, unsigned short pgn, unsigned char srcAddr, void *data, unsigned char size = 8);
-		bool send(unsigned short id, void *data, unsigned char size = 8);
-	};
+    const unsigned int *mRxFifo0[3];
+    const unsigned int *mTxbuf[3];
+    unsigned char mRxFifoIndex0;
+
+    FDCAN_GlobalTypeDef *mPeri;
+    Mutex mMutex;
+
+  public:
+    Can(FDCAN_GlobalTypeDef *peri, void (*clockFunc)(bool en), void (*nvicFunc)(bool en), unsigned int (*getClockFreq)(void));
+    bool init(unsigned int baudRate, unsigned int bufDepth, float samplePoint = 0.875);
+    void push(unsigned int *data);
+    void isr(void);
+    bool isReceived(void);
+    bool isStandard(void);
+    unsigned int getIdentifier(void);
+    unsigned char getPriority(void);
+    unsigned short getPgn(void);
+    unsigned char getSrcAddr(void);
+    unsigned char getSize(void);
+    char *getData(void);
+    void flush(void);
+    void releaseFifo(void);
+    bool send(unsigned char priority, unsigned short pgn, unsigned char srcAddr, void *data, unsigned char size = 8);
+    bool send(unsigned short id, void *data, unsigned char size = 8);
+};
 }
 
 #if defined(CAN1_ENABLE) && defined(FDCAN1)
