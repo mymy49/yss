9314b90f1bc556dd6b8c5fc38d6c2319632bdd54
Yoon-Ki Hong <mymy49@nate.com>
2021-10-13 19:30:18 +0900

    - protocol/Endpoint 추가

---
+++ yss/inc/protocol/Endpoint.h
@@ -0,0 +1,57 @@
+////////////////////////////////////////////////////////////////////////////////////////
+//
+// 저작권 표기 License_ver_2.0
+// 본 소스코드의 소유권은 yss Embedded Operating System 네이버 카페 관리자와 운영진에게 있습니다.
+// 운영진이 임의로 코드의 권한을 타인에게 양도할 수 없습니다.
+// 본 소스코드는 아래 사항에 동의할 경우에 사용 가능합니다.
+// 아래 사항에 대해 동의하지 않거나 이해하지 못했을 경우 사용을 금합니다.
+// 본 소스코드를 사용하였다면 아래 사항을 모두 동의하는 것으로 자동 간주 합니다.
+// 본 소스코드의 상업적 또는 비상업적 이용이 가능합니다.
+// 본 소스코드의 내용을 임의로 수정하여 재배포하는 행위를 금합니다.
+// 본 소스코드의 내용을 무단 전재하는 행위를 금합니다.
+// 본 소스코드의 사용으로 인해 발생하는 모든 사고에 대해서 어떤한 법적 책임을 지지 않습니다.
+//
+//  Home Page : http://cafe.naver.com/yssoperatingsystem
+//  Copyright 2021. yss Embedded Operating System all right reserved.
+//
+//  주담당자 : 아이구 (mymy49@nate.com) 2021.10.04~ 현재
+//  부담당자 : -
+//
+////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef YSS_PROTOCOL_ENDPOINT__H_
+#define YSS_PROTOCOL_ENDPOINT__H_
+
+#include <drv/drv_Uart.h>
+#include <yss/Mutex.h>
+
+class Fifo;
+
+class Endpoint
+{
+  public:
+    enum
+    {
+        MAX_ENDPOINT_NUM = 8
+    };
+
+  private:
+    drv::Uart *mUart;
+    signed int mSenderThreadId, mReceiverThreadId, mBufSize;
+    unsigned char mNumOfEndpoint;
+    unsigned char mRcvBuf[254];
+    Fifo *mTxFifo[MAX_ENDPOINT_NUM], *mRxFifo[MAX_ENDPOINT_NUM];
+    Mutex mMutex;
+
+  public:
+    Endpoint(drv::Uart &uart, unsigned char numOfEndpoint, unsigned int fifoSize);
+    ~Endpoint(void);
+    void init(void);
+    void processSender(void);
+    void processReceiver(void);
+    unsigned char getWaitUntilReceive(unsigned char endpoint);
+    signed short get(unsigned char endpoint);
+    void send(unsigned char endpoint, const void *src, unsigned int len);
+};
+
+#endif
