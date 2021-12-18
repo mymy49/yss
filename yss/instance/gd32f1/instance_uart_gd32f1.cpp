af07b8b4607a692662a24a4b216bf07128032685
Yoon-Ki Hong <mymy49@nate.com>
2021-12-19 08:30:40 +0900

    - 현재까지 작성된 GD32F1의 instance 분리 작업

--- yss/instance/gd32f1/instance_uart_gd32f1.cpp
+++ yss/instance/gd32f1/instance_uart_gd32f1.cpp
@@ -22,11 +22,9 @@
 #include <drv/mcu.h>
 #include <yss/instance.h>
 
-#if defined(STM32F1) || defined(STM32F4) || defined(STM32F7)
+#if defined(GD32F10X_XD)
 
 #include <config.h>
-
-#if defined(STM32F1) || defined(STM32F4) || defined(STM32F7) || defined(STM32L0) || defined(STM32G4) || defined(STM32F0) || defined(GD32F10X_XD)
 
 static unsigned int getApb2ClkFreq(void)
 {
@@ -37,10 +35,6 @@ static unsigned int getApb1ClkFreq(void)
 {
 	return clock.getApb1ClkFreq();
 }
-
-#elif defined(__SAM_L_FAMILY)
-
-#endif
 
 #if defined(USART1) && defined(UART1_ENABLE)
 static void setUart1ClockEn(bool en)
@@ -58,11 +52,8 @@ static void resetUart1(void)
 	clock.peripheral.resetUart1();
 }
 
-#if defined(STM32F1) || defined(STM32F4) || defined(STM32F7) || defined(STM32L0) || defined(STM32F0) || defined(GD32F10X_XD)
 drv::Uart uart1(USART1, setUart1ClockEn, setUart1IntEn, resetUart1, YSS_DMA_MAP_UART1_TX_STREAM, YSS_DMA_MAP_UART1_TX_CHANNEL, define::dma::priorityLevel::LOW, getApb2ClkFreq);
-#elif defined(STM32G4)
-drv::Uart uart1(USART1, setUart1ClockEn, setUart1IntEn, resetUart1, getApb2ClkFreq);
-#endif
+
 extern "C"
 {
 	void USART1_IRQHandler(void)
@@ -88,11 +79,7 @@ static void resetUart2(void)
 	clock.peripheral.resetUart2();
 }
 
-#if defined(STM32F1) || defined(STM32F4) || defined(STM32F7) || defined(STM32L0) || defined(STM32F0) || defined(GD32F10X_XD)
 drv::Uart uart2(USART2, setUart2ClockEn, setUart2IntEn, resetUart2, YSS_DMA_MAP_UART2_TX_STREAM, YSS_DMA_MAP_UART2_TX_CHANNEL, define::dma::priorityLevel::LOW, getApb1ClkFreq);
-#elif defined(STM32G4)
-drv::Uart uart2(USART1, setUart2ClockEn, setUart2IntEn, resetUart2, getApb1ClkFreq);
-#endif
 
 extern "C"
 {
@@ -120,12 +107,7 @@ static void resetUart3(void)
 	clock.peripheral.resetUart3();
 }
 
-#if defined(STM32F1) || defined(STM32F4) || defined(STM32F7) || defined(STM32L0) || defined(STM32F0) || defined(GD32F10X_XD)
 drv::Uart uart3(USART3, setUart3ClockEn, setUart3IntEn, resetUart3, YSS_DMA_MAP_UART3_TX_STREAM, YSS_DMA_MAP_UART3_TX_CHANNEL, define::dma::priorityLevel::LOW, getApb1ClkFreq);
-#elif defined(STM32G4)
-drv::Uart uart3(USART3, setUart3ClockEn, setUart3IntEn, resetUart3, getApb1ClkFreq);
-#endif
-
 
 extern "C"
 {
@@ -153,11 +135,7 @@ static void resetUart4(void)
 	clock.peripheral.resetUart4();
 }
 
-#if defined(STM32F1) || defined(STM32F4) || defined(STM32F7) || defined(STM32L0) || defined(STM32F0) || defined(GD32F10X_XD)
 drv::Uart uart4(UART4, setUart4ClockEn, setUart4IntEn, resetUart4, YSS_DMA_MAP_UART4_TX_STREAM, YSS_DMA_MAP_UART4_TX_CHANNEL, define::dma::priorityLevel::LOW, getApb1ClkFreq);
-#elif defined(STM32G4)
-drv::Uart uart4(UART4, setUart4ClockEn, setUart4IntEn, resetUart4, getApb1ClkFreq);
-#endif
 
 extern "C"
 {
@@ -184,113 +162,13 @@ static void resetUart5(void)
 	clock.peripheral.resetUart5();
 }
 
-#if defined(STM32F1) || defined(STM32F4) || defined(STM32F7) || defined(STM32L0) || defined(STM32F0) || defined(GD32F10X_XD)
 drv::Uart uart5(UART5, setUart5ClockEn, setUart5IntEn, resetUart5, YSS_DMA_MAP_UART5_TX_STREAM, YSS_DMA_MAP_UART5_TX_CHANNEL, define::dma::priorityLevel::LOW, getApb1ClkFreq);
-#elif defined(STM32G4)
-drv::Uart uart5(UART5, setUart5ClockEn, setUart5IntEn, resetUart5, getApb1ClkFreq);
-#endif
 
 extern "C"
 {
 	void UART5_IRQHandler(void)
 	{
 		uart5.isr();
-	}
-}
-
-#endif
-
-#if defined(USART6) && defined(UART6_ENABLE)
-static void setUart6ClockEn(bool en)
-{
-	clock.peripheral.setUart6En(en);
-}
-
-static void setUart6IntEn(bool en)
-{
-	nvic.setUart6En(en);
-}
-
-static void resetUart6(void)
-{
-	clock.peripheral.resetUart6();
-}
-
-#if defined(STM32F1) || defined(STM32F4) || defined(STM32F7) || defined(STM32L0) || defined(STM32F0) || defined(GD32F10X_XD)
-drv::Uart uart6(USART6, setUart6ClockEn, setUart6IntEn, resetUart6, YSS_DMA_MAP_UART6_TX_STREAM, YSS_DMA_MAP_UART6_TX_CHANNEL, define::dma::priorityLevel::LOW, getApb2ClkFreq);
-#elif defined(STM32G4)
-drv::Uart uart6(USART6, setUart6ClockEn, setUart6IntEn, resetUart6, getApb2ClkFreq);
-#endif
-
-extern "C"
-{
-	void USART6_IRQHandler(void)
-	{
-		uart6.isr();
-	}
-}
-
-#endif
-
-#if defined(UART7) && defined(UART7_ENABLE)
-static void setUart7ClockEn(bool en)
-{
-	clock.peripheral.setUart7En(en);
-}
-
-static void setUart7IntEn(bool en)
-{
-	nvic.setUart7En(en);
-}
-
-static void resetUart7(void)
-{
-	clock.peripheral.resetUart7();
-}
-
-#if defined(STM32F1) || defined(STM32F4) || defined(STM32F7) || defined(STM32L0) || defined(STM32F0) || defined(GD32F10X_XD)
-drv::Uart uart7(UART7, setUart7ClockEn, setUart7IntEn, resetUart7, YSS_DMA_MAP_UART7_TX_STREAM, YSS_DMA_MAP_UART7_TX_CHANNEL, define::dma::priorityLevel::LOW, getApb1ClkFreq);
-#elif defined(STM32G4)
-drv::Uart uart7(UART7, setUart7ClockEn, setUart7IntEn, resetUart7, getApb1ClkFreq);
-#endif
-
-extern "C"
-{
-	void UART7_IRQHandler(void)
-	{
-		uart7.isr();
-	}
-}
-
-#endif
-
-#if defined(UART8) && defined(UART8_ENABLE)
-static void setUart8ClockEn(bool en)
-{
-	clock.peripheral.setUart8En(en);
-}
-
-static void setUart8IntEn(bool en)
-{
-	nvic.setUart8En(en);
-}
-
-static void resetUart8(void)
-{
-	clock.peripheral.resetUart8();
-}
-
-#if defined(STM32F1) || defined(STM32F4) || defined(STM32F7) || defined(STM32L0) || defined(STM32F0) || defined(GD32F10X_XD)
-drv::Uart uart8(UART8, setUart8ClockEn, setUart8IntEn, resetUart8, YSS_DMA_MAP_UART8_TX_STREAM, YSS_DMA_MAP_UART8_TX_CHANNEL, define::dma::priorityLevel::LOW, getApb1ClkFreq);
-#elif defined(STM32G4)
-drv::Uart uart8(UART8, setUart8ClockEn, setUart8IntEn, resetUart8, getApb1ClkFreq);
-#endif
-
-extern "C"
-{
-	void UART8_IRQHandler(void)
-	{
-		uart8.isr();
 	}
 }
 
