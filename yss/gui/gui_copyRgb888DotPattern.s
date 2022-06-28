5953b4f3a670ed2a4b31fe1cf34e9c1ca4434d9d
Yoon-Ki Hong <mymy49@nate.com>
2022-06-28 23:26:40 +0900

    [GUI 속도 개선] - copyRgb888dotPattern() 함수 추가

--- yss/gui/gui_copyRgb888DotPattern.s
+++ yss/gui/gui_copyRgb888DotPattern.s
@@ -16,22 +16,23 @@
 //
 ////////////////////////////////////////////////////////////////////////////////////////
 
-#if defined(STM32F746xx) || defined(STM32F745xx) || \
-	defined(STM32F765xx) || defined(STM32F767xx) || defined(STM32F768xx) || defined(STM32F769xx) || \
-	defined(STM32F405xx) ||	defined(STM32F415xx) ||	\
-	defined(STM32F407xx) ||	defined(STM32F417xx) ||	\
-	defined(STM32F427xx) ||	defined(STM32F437xx) ||	\
-	defined(STM32F429xx) ||	defined(STM32F439xx) || \
-	defined(STM32F100xB) || defined(STM32F100xE) || \
-	defined(STM32F101x6) || defined(STM32F101xB) || defined(STM32F101xE) || defined(STM32F101xG) || \
-	defined(STM32F102x6) || defined(STM32F102xB) || \
-	defined(STM32F103x6) || defined(STM32F103xB) || defined(STM32F103xE) || defined(STM32F103xG) || \
-	defined(STM32F105xC) || \
-	defined(STM32F107xC) || \
-	defined(STM32L151xB) || \
+#if defined (STM32F746xx) || defined(STM32F745xx) || \
+	defined (STM32F765xx) || defined(STM32F767xx) || defined(STM32F768xx) || defined(STM32F769xx) || \
+	defined (STM32F405xx) ||	defined(STM32F415xx) ||	\
+	defined (STM32F407xx) ||	defined(STM32F417xx) ||	\
+	defined (STM32F427xx) ||	defined(STM32F437xx) ||	\
+	defined (STM32F429xx) ||	defined(STM32F439xx) || \
+	defined (STM32F100xB) || defined(STM32F100xE) || \
+	defined (STM32F101x6) || defined(STM32F101xB) || defined(STM32F101xE) || defined(STM32F101xG) || \
+	defined (STM32F102x6) || defined(STM32F102xB) || \
+	defined (STM32F103x6) || defined(STM32F103xB) || defined(STM32F103xE) || defined(STM32F103xG) || \
+	defined (STM32F105xC) || \
+	defined (STM32F107xC) || \
+	defined (STM32L151xB) || \
 	defined (STM32G431xx) || defined (STM32G441xx) || \
 	defined (STM32G471xx) || defined (STM32G473xx) || defined (STM32G474xx) || defined (STM32G483xx) || defined (STM32G484xx) || defined (STM32GBK1CB) || \
-	defined(GD32F10X_XD) || defined(GD32F10X_HD) || defined(GD32F450)
+	defined (GD32F10X_XD) || \
+	defined(GD32F10X_XD) || defined(GD32F10X_HD)
 
 #define __CORE_CM3_CM4_CM7_H_GENERIC
 #else
@@ -48,113 +49,66 @@
 copyRgb888DotPattern:
 	push {r3-r7}
 	bic r1, #0xff000000
-	bne unalignedRepeat
-	mov r4, r1
-	mov r5, r4
-	mov r6, r4
-	mov r7, r4
+	mov r5, r1
+	mov r6, #0
+	mov r7, #0
+	lsl r3, r1, #24
+	orr r5, r3
+	lsr r6, r1, #8
+	lsl r3, r1, #16
+	orr r6, r3
+	lsr r7, r1, #16
+	lsl r3, r1, #8
+	orr r7, r3
 repeat:
-	movs r1, #256
-	subs r3, r2, r1
+	subs r3, r2, #64
 	bge greater
-	ands r3, r2, #0xf
-	bic r2, #0xf
-	subs r1, r1, r2 
-	lsr r1, r1, #3
-	adds r1, r1, #2
-	movs r2, r3
-	add pc, pc, r1
+	mov r3, #64
+	subs r4, r3, r2
+	mov r3, #4
+	udiv r4, r4, r3
+	lsl r4, #1
+	bic r2, #0xFFFFFFFC
+	add r4, #2
+	add pc, pc, r4
 greater:
-	subs r2, #256
-	stmia   R0!,{R4-R7}
-	stmia   R0!,{R4-R7}
-	stmia   R0!,{R4-R7}
-	stmia   R0!,{R4-R7}
-	stmia   R0!,{R4-R7}
-	stmia   R0!,{R4-R7}
-	stmia   R0!,{R4-R7}
-	stmia   R0!,{R4-R7}
-	stmia   R0!,{R4-R7}
-	stmia   R0!,{R4-R7}
-	stmia   R0!,{R4-R7}
-	stmia   R0!,{R4-R7}
-	stmia   R0!,{R4-R7}
-	stmia   R0!,{R4-R7}
-	stmia   R0!,{R4-R7}
-	stmia   R0!,{R4-R7}
-	subs r3, r2, #16
+	subs r2, #64
+	stmia   R0!,{R5-R7}
+	stmia   R0!,{R5-R7}
+	stmia   R0!,{R5-R7}
+	stmia   R0!,{R5-R7}
+	stmia   R0!,{R5-R7}
+	stmia   R0!,{R5-R7}
+	stmia   R0!,{R5-R7}
+	stmia   R0!,{R5-R7}
+	stmia   R0!,{R5-R7}
+	stmia   R0!,{R5-R7}
+	stmia   R0!,{R5-R7}
+	stmia   R0!,{R5-R7}
+	stmia   R0!,{R5-R7}
+	stmia   R0!,{R5-R7}
+	stmia   R0!,{R5-R7}
+	stmia   R0!,{R5-R7}
+	subs r3, r2, #4
 	bhi repeat
 	movs r2, r2
 	beq finish
-	subs r3, r2, #8
-	blt sub4
-	stmia   R0!,{R4-R5}
-	subs r2, #8
-sub4:
-	subs r3, r2, #4
-	blt sub2
-	str r4, [r0], #4
-	subs r2, #4
-sub2:
-	subs r3, r2, #2
-	blt sub1
-	strh r4, [r0], #2
-	subs r2, #2
-sub1:
+	strb r5, [r0], #1
+	strb r6, [r0], #1
+	strb r7, [r0], #1
+	sub r2, #1
 	movs r2, r2
 	beq finish
-	strb r4, [r0], #1
+	strb r5, [r0], #1
+	strb r6, [r0], #1
+	strb r7, [r0], #1
+	sub r2, #1
+	movs r2, r2
+	beq finish
+	strb r5, [r0], #1
+	strb r6, [r0], #1
+	strb r7, [r0], #1
 finish:
 	pop {r3-r7}
 	bx lr
-
-unalignedRepeat :
-	movs r4, #128
-	subs r3, r2, r4
-	bge greaterUnaligned
-	ands r3, r2, #0x3
-	bic r2, #0x3
-	subs r4, r4, r2 
-//	lsls r4, r4, #1
-	movs r2, r3
-	add pc, pc, r4
-greaterUnaligned:
-	subs r2, #128
-	str r1, [r0], #4
-	str r1, [r0], #4
-	str r1, [r0], #4
-	str r1, [r0], #4
-	str r1, [r0], #4
-	str r1, [r0], #4
-	str r1, [r0], #4
-	str r1, [r0], #4
-	str r1, [r0], #4
-	str r1, [r0], #4
-	str r1, [r0], #4
-	str r1, [r0], #4
-	str r1, [r0], #4
-	str r1, [r0], #4
-	str r1, [r0], #4
-	str r1, [r0], #4
-	str r1, [r0], #4
-	str r1, [r0], #4
-	str r1, [r0], #4
-	str r1, [r0], #4
-	str r1, [r0], #4
-	str r1, [r0], #4
-	str r1, [r0], #4
-	str r1, [r0], #4
-	str r1, [r0], #4
-	str r1, [r0], #4
-	str r1, [r0], #4
-	str r1, [r0], #4
-	str r1, [r0], #4
-	str r1, [r0], #4
-	str r1, [r0], #4
-	str r1, [r0], #4
-	subs r3, r2, #4
-	bhi unalignedRepeat
-	b sub2	
-	.endfunc
-
 #endif
