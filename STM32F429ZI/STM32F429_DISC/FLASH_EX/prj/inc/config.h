2ac46b70f462e63223ed34a905e82387096c17c9
Yoon-Ki Hong <mymy49@nate.com>
2022-09-04 16:43:29 +0900

    [STM32F4xx FLASH 예제 수정] - 신규 업데이트 사항의 누락 코드 수정

--- STM32F429ZI/STM32F429_DISC/FLASH_EX/prj/inc/config.h
+++ STM32F429ZI/STM32F429_DISC/FLASH_EX/prj/inc/config.h
@@ -1,21 +1,18 @@
 ////////////////////////////////////////////////////////////////////////////////////////
 //
-// 저작권 표기 License_ver_2.0
-// 본 소스코드의 소유권은 yss Embedded Operating System 네이버 카페 관리자와 운영진에게 있습니다.
-// 운영진이 임의로 코드의 권한을 타인에게 양도할 수 없습니다.
-// 본 소스코드는 아래 사항에 동의할 경우에 사용 가능합니다.
+// 저작권 표기 License_ver_3.0
+// 본 소스 코드의 소유권은 홍윤기에게 있습니다.
+// 어떠한 형태든 기여는 기증으로 받아들입니다.
+// 본 소스 코드는 아래 사항에 동의할 경우에 사용 가능합니다.
 // 아래 사항에 대해 동의하지 않거나 이해하지 못했을 경우 사용을 금합니다.
-// 본 소스코드를 사용하였다면 아래 사항을 모두 동의하는 것으로 자동 간주 합니다.
-// 본 소스코드의 상업적 또는 비상업적 이용이 가능합니다.
-// 본 소스코드의 내용을 임의로 수정하여 재배포하는 행위를 금합니다.
-// 본 소스코드의 내용을 무단 전재하는 행위를 금합니다.
-// 본 소스코드의 사용으로 인해 발생하는 모든 사고에 대해서 어떤한 법적 책임을 지지 않습니다.
+// 본 소스 코드를 사용하였다면 아래 사항을 모두 동의하는 것으로 자동 간주 합니다.
+// 본 소스 코드의 상업적 또는 비 상업적 이용이 가능합니다.
+// 본 소스 코드의 내용을 임의로 수정하여 재배포하는 행위를 금합니다.
+// 본 소스 코드의 내용을 무단 전재하는 행위를 금합니다.
+// 본 소스 코드의 사용으로 인해 발생하는 모든 사고에 대해서 어떠한 법적 책임을 지지 않습니다.
 //
-//	Home Page : http://cafe.naver.com/yssoperatingsystem
-//	Copyright 2020.	yss Embedded Operating System all right reserved.
-//  
-//  주담당자 : 아이구 (mymy49@nate.com) 2016.04.30 ~ 현재
-//  부담당자 : -
+// Home Page : http://cafe.naver.com/yssoperatingsystem
+// Copyright 2022. 홍윤기 all right reserved.
 //
 ////////////////////////////////////////////////////////////////////////////////////////
 
@@ -23,39 +20,6 @@
 #define YSS_CONFIG__H_
 
 #define HSE_CLOCK_FREQ					8000000
-
-#define YSS_USE_DEFAULT_MSP				true
-
-// ####################### hmalloc 설정 #######################
-
-// SRAM을 이용한 동적할당 메모리의 사용 여부(true, false)
-#define	YSS_H_HEAP_USE					true
-
-// hmalloc의	총 메모리 용량 설정
-#define	YSS_H_HEAP_SIZE					(64 * 1024)
-
-// hmalloc의	기본 할당 단위
-#define	YSS_H_HEAP_CLUSTER_SIZE			64
-
-// hmalloc의	최대 할당 개수
-#define	YSS_H_MAX_NUM_OF_MALLOC			256
-
-// hmalloc의 전체 클러스터 용량(수정 금지)
-#define	YSS_H_HEAP_TOTAL_CLUSTER_SIZE	(YSS_H_HEAP_SIZE / YSS_H_HEAP_CLUSTER_SIZE / 32)
-
-#if YSS_H_HEAP_SIZE % YSS_H_HEAP_CLUSTER_SIZE
-#error "YSS_H_HEAP_SIZE가 YSS_H_HEAP_CLUSTER_SIZE로 나누어 떨어지게 설정해주세요."
-#endif
-
-#if YSS_H_HEAP_CLUSTER_SIZE % 4
-#error "YSS_H_HEAP_CLUSTER_SIZE 4로 나누어 떨어지게 설정해주세요."
-#endif
-
-#if YSS_H_HEAP_SIZE / YSS_H_HEAP_CLUSTER_SIZE < 32
-#error "YSS_H_HEAP_SIZE의 값이 YSS_H_HEAP_CLUSTER_SIZE로 나누어 32보다 작지 않게 해주세요."
-#endif
-
-
 
 // ####################### lmalloc 설정 #######################
 
@@ -66,19 +30,19 @@
 // SDRAM의 시작 주소 설정
 // STM32F7 (Bank1 - 0x60000000, Bank2 - 0x70000000)
 // STM32F4 (Bank1 - 0xC0000000, Bank2 - 0xD0000000)
-#define	YSS_SDRAM_ADDR					0xD0000000
+#define YSS_SDRAM_ADDR					0xD0000000
 
 // SDRAM의 총 메모리 용량 설정
-#define	YSS_L_HEAP_SIZE					(8 * 1024 * 1024)
+#define YSS_L_HEAP_SIZE					(8 * 1024 * 1024)
 
 // lmalloc의	기본 할당 단위
-#define	YSS_L_HEAP_CLUSTER_SIZE			(256)
+#define YSS_L_HEAP_CLUSTER_SIZE			(256)
 
 // lmalloc의	최대 할당 개수
-#define	YSS_L_MAX_NUM_OF_MALLOC			1024
+#define YSS_L_MAX_NUM_OF_MALLOC			1024
 
 // lmalloc의 내부 계산 식(수정 금지)
-#define	YSS_L_HEAP_TOTAL_CLUSTER_SIZE		(YSS_L_HEAP_SIZE / YSS_L_HEAP_CLUSTER_SIZE / 32)
+#define YSS_L_HEAP_TOTAL_CLUSTER_SIZE		(YSS_L_HEAP_SIZE / YSS_L_HEAP_CLUSTER_SIZE / 32)
 #define YSS_L_HEAP_CLUSTER_BASE_ADDR		(YSS_SDRAM_ADDR)
 #define YSS_L_HEAP_TABLE_BASE_ADDR			(YSS_L_HEAP_CLUSTER_BASE_ADDR + YSS_L_HEAP_TOTAL_CLUSTER_SIZE * sizeof(long))
 #define YSS_L_HEAP_BASE_ADDR				(YSS_L_HEAP_TABLE_BASE_ADDR + YSS_L_MAX_NUM_OF_MALLOC * 12)
@@ -102,26 +66,6 @@
 
 
 
-// ####################### NEW 예약어 지원 설정 #######################
-
-#define YSS_H_HEAP		1
-#define YSS_L_HEAP		2
-
-// new 예약어의 할당 메모리 설정 (YSS_H_HEAP, YSS_L_HEAP)
-#if YSS_L_HEAP_USE == true && !defined(YSS_NEW_DELETE_USING_HEAP)
-#define YSS_NEW_DELETE_USING_HEAP	YSS_H_HEAP
-#else
-#define YSS_NEW_DELETE_USING_HEAP	YSS_H_HEAP
-#endif
-
-#if YSS_NEW_DELETE_USING_HEAP == YSS_H_HEAP && YSS_H_HEAP_USE == false
-#error "YSS_NEW_DELETE_USING_HEAP YSS_H_HEAP으로 설정되어 있으나 YSS_H_HEAP이 비활성화되어 있습니다."
-#elif YSS_NEW_DELETE_USING_HEAP == YSS_L_HEAP && YSS_L_HEAP_USE == false
-#error "YSS_NEW_DELETE_USING_HEAP YSS_L_HEAP으로 설정되어 있으나 YSS_L_HEAP이 비활성화되어 있습니다."
-#endif
-
-
-
 // ####################### 스케줄러 설정 #######################
 
 // 내부 ms 를 만들 시계의 타이머 설정 (timer1 ~ timer14)
@@ -132,15 +76,6 @@
 
 // 최대 등록 가능한 쓰레드의 수
 #define MAX_THREAD						64
-
-// 쓰레드 스택의 배치 메모리 (YSS_H_HEAP, YSS_L_HEAP)
-#define THREAD_STACK_ALLOCATION_PLACE	YSS_H_HEAP
-
-#if THREAD_STACK_ALLOCATION_PLACE == YSS_H_HEAP && YSS_H_HEAP_USE == false
-#error "THREAD_STACK_ALLOCATION_PLACE이 YSS_H_HEAP으로 설정되어 있으나 YSS_H_HEAP이 비활성화되어 있습니다."
-#elif THREAD_STACK_ALLOCATION_PLACE == YSS_L_HEAP && YSS_L_HEAP_USE == false
-#error "THREAD_STACK_ALLOCATION_PLACE이 YSS_L_HEAP으로 설정되어 있으나 YSS_L_HEAP이 비활성화되어 있습니다."
-#endif
 
 
 
