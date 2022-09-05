23d0080d4930a0247b2807f120b6f4cd6bcdbfb6
Yoon-Ki Hong <mymy49@nate.com>
2022-09-05 18:37:21 +0900

    [STM32F7xx GUI_Button_EX 예제] - 최근 업데이트 누락 사항 수정

--- STM32F746NG/STM32F746_DISC/GUI_Button_EX/prj/src/main.cpp
+++ STM32F746NG/STM32F746_DISC/GUI_Button_EX/prj/src/main.cpp
@@ -1,21 +1,18 @@
 ////////////////////////////////////////////////////////////////////////////////////////
 //
-// 저작권 표기 License_ver_2.0
-// 본 소스코드의 소유권은 yss Embedded Operating System 네이버 카페 관리자와 운영진에게 있습니다.
-// 운영진이 임의로 코드의 권한을 타인에게 양도할 수 없습니다.
-// 본 소스코드는 아래 사항에 동의할 경우에 사용 가능합니다.
+// 저작권 표기 License_ver_3.0
+// 본 소스 코드의 소유권은 홍윤기에게 있습니다.
+// 어떠한 형태든 기여는 기증으로 받아들입니다.
+// 본 소스 코드는 아래 사항에 동의할 경우에 사용 가능합니다.
 // 아래 사항에 대해 동의하지 않거나 이해하지 못했을 경우 사용을 금합니다.
-// 본 소스코드를 사용하였다면 아래 사항을 모두 동의하는 것으로 자동 간주 합니다.
-// 본 소스코드의 상업적 또는 비상업적 이용이 가능합니다.
-// 본 소스코드의 내용을 임의로 수정하여 재배포하는 행위를 금합니다.
-// 본 소스코드의 내용을 무단 전재하는 행위를 금합니다.
-// 본 소스코드의 사용으로 인해 발생하는 모든 사고에 대해서 어떤한 법적 책임을 지지 않습니다.
+// 본 소스 코드를 사용하였다면 아래 사항을 모두 동의하는 것으로 자동 간주 합니다.
+// 본 소스 코드의 상업적 또는 비 상업적 이용이 가능합니다.
+// 본 소스 코드의 내용을 임의로 수정하여 재배포하는 행위를 금합니다.
+// 본 소스 코드의 내용을 무단 전재하는 행위를 금합니다.
+// 본 소스 코드의 사용으로 인해 발생하는 모든 사고에 대해서 어떠한 법적 책임을 지지 않습니다.
 //
-//	Home Page : http://cafe.naver.com/yssoperatingsystem
-//	Copyright 2020.	yss Embedded Operating System all right reserved.
-//
-//  주담당자 : 아이구 (mymy49@nate.com) 2019.12.22 ~ 현재
-//  부담당자 : -
+// Home Page : http://cafe.naver.com/yssoperatingsystem
+// Copyright 2022. 홍윤기 all right reserved.
 //
 ////////////////////////////////////////////////////////////////////////////////////////
 
@@ -28,37 +25,37 @@ Frame *gFrame;
 
 void handler_red(void)
 {
-	gFrame->setBgColor(255, 0, 0);
+	gFrame->setBackgroundColor(255, 0, 0);
 }
 
 void handler_yellow(void)
 {
-	gFrame->setBgColor(255, 255, 0);
+	gFrame->setBackgroundColor(255, 255, 0);
 }
 
 void handler_white(void)
 {
-	gFrame->setBgColor(255, 255, 255);
+	gFrame->setBackgroundColor(255, 255, 255);
 }
 
 void handler_black(void)
 {
-	gFrame->setBgColor(0, 0, 0);
+	gFrame->setBackgroundColor(0, 0, 0);
 }
 
 void handler_blue(void)
 {
-	gFrame->setBgColor(0, 0, 255);
+	gFrame->setBackgroundColor(0, 0, 255);
 }
 
 void handler_magenta(void)
 {
-	gFrame->setBgColor(0, 255, 255);
+	gFrame->setBackgroundColor(0, 255, 255);
 }
 
 void handler_green(void)
 {
-	gFrame->setBgColor(0, 255, 0);
+	gFrame->setBackgroundColor(0, 255, 0);
 }
 
 int main(void)
@@ -66,58 +63,58 @@ int main(void)
 	yss::init();
 	bsp::init();
 
-	gFrame = new Frame();
-	gFrame->setBgColor(0x00, 0x00, 0x00);
+	gFrame = new Frame;
+	gFrame->setBackgroundColor(0x00, 0x00, 0x00);
 
 	Button redBtn, yelloBtn, whiteBtn, blackBtn, blueBtn, magentaBtn, greenBtn;
 
 	redBtn.setFont(Font_Ubuntu_12_B);
 	redBtn.setSize(50, 30);
-	redBtn.setPos(20, 20);
-	redBtn.setBgColor(255, 0, 0);
+	redBtn.setPosition(20, 20);
+	redBtn.setBackgroundColor(255, 0, 0);
 	redBtn.setText("적색");
 	redBtn.setPushEventHandler(handler_red);
 
 	yelloBtn.setFont(Font_Ubuntu_12_B);
 	yelloBtn.setSize(50, 30);
-	yelloBtn.setPos(90, 20);
-	yelloBtn.setBgColor(255, 255, 0);
+	yelloBtn.setPosition(90, 20);
+	yelloBtn.setBackgroundColor(255, 255, 0);
 	yelloBtn.setText("황색");
 	yelloBtn.setPushEventHandler(handler_yellow);
 
 	whiteBtn.setFont(Font_Ubuntu_12_B);
 	whiteBtn.setSize(50, 30);
-	whiteBtn.setPos(20, 70);
-	whiteBtn.setBgColor(255, 255, 255);
+	whiteBtn.setPosition(20, 70);
+	whiteBtn.setBackgroundColor(255, 255, 255);
 	whiteBtn.setText("백색");
 	whiteBtn.setPushEventHandler(handler_white);
 
 	blackBtn.setFont(Font_Ubuntu_12_B);
 	blackBtn.setSize(50, 30);
-	blackBtn.setPos(90, 70);
-	blackBtn.setBgColor(0, 0, 0);
+	blackBtn.setPosition(90, 70);
+	blackBtn.setBackgroundColor(0, 0, 0);
 	blackBtn.setFontColor(255, 255, 255);
 	blackBtn.setText("흑색");
 	blackBtn.setPushEventHandler(handler_black);
 
 	blueBtn.setFont(Font_Ubuntu_12_B);
 	blueBtn.setSize(50, 30);
-	blueBtn.setPos(20, 120);
-	blueBtn.setBgColor(0, 0, 255);
+	blueBtn.setPosition(20, 120);
+	blueBtn.setBackgroundColor(0, 0, 255);
 	blueBtn.setText("청색");
 	blueBtn.setPushEventHandler(handler_blue);
 
 	magentaBtn.setFont(Font_Ubuntu_12_B);
 	magentaBtn.setSize(50, 30);
-	magentaBtn.setPos(90, 120);
-	magentaBtn.setBgColor(0, 255, 255);
+	magentaBtn.setPosition(90, 120);
+	magentaBtn.setBackgroundColor(0, 255, 255);
 	magentaBtn.setText("자색");
 	magentaBtn.setPushEventHandler(handler_magenta);
 
 	greenBtn.setFont(Font_Ubuntu_12_B);
 	greenBtn.setSize(50, 30);
-	greenBtn.setPos(20, 170);
-	greenBtn.setBgColor(0, 255, 0);
+	greenBtn.setPosition(20, 170);
+	greenBtn.setBackgroundColor(0, 255, 0);
 	greenBtn.setText("녹색");
 	greenBtn.setPushEventHandler(handler_green);
 
@@ -133,6 +130,14 @@ int main(void)
 
 	while (1)
 	{
+		//greenBtn.setText("녹색");
+		//greenBtn.paint();
+		//greenBtn.update();
+		//thread::delay(1000);
+		//greenBtn.setText("Green");
+		//greenBtn.paint();
+		//greenBtn.update();
+		//thread::delay(1000);
 		thread::yield();
 	}
 
