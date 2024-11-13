5e4c019dfcbcb2b5cebf05ed200596e3feaa00f5
Yoon-Ki Hong <mymy49@nate.com>
2024-11-13 21:01:18 +0900

    [STM32H7 Clock 업데이트] - enablePll1() 함수에서 PLL이 활성화 될 때까지 기다리는 실수로 빠진 코드 추가 - 주석을 Crossworks for ARM의 팝업에 알맞게 나오도록 수정

--- inc/targets/st/class_clock_stm32h7.h
+++ inc/targets/st/class_clock_stm32h7.h
@@ -12,18 +12,21 @@
 #include <drv/peripheral.h>
 #include <yss/Mutex.h>
 
+/*
+	.
+	MCU의 Clock을 설정하는 class 입니다.
+*/
 class Clock : public Mutex
 {
 public:
-	// 외부 HSE 크리스탈을 활성화 합니다.
-	//
-	// 반환
-	//		에러를 반환합니다.
-	// uint32_t hseHz
-	//		외부 크리스털의 주파수를 HZ 단위로 입력합니다.
-	// useBypass = false
-	//		입력이 크리스탈일 경우에는 false로 설정합니다.
-	//		입력이 오실레이터일 경우나 클럭 소스를 직접 입력 받을 경우 bypass를 true로 설정합니다.
+	/* 
+		외부 HSE 크리스탈을 활성화 합니다.
+		.
+		@ return : 에러를 반환합니다.
+		.
+		@ hseHz : 외부 크리스털의 주파수를 HZ 단위로 입력합니다.
+		@ useBypass : 입력이 크리스탈일 경우에는 false로 설정합니다. 입력이 오실레이터일 경우나 클럭 소스를 직접 입력 받을 경우 bypass를 true로 설정합니다.
+	*/
 	error_t enableHse(uint32_t hseHz, bool useBypass = false) __attribute__((optimize("-O1")));
 	
 	typedef enum
@@ -34,13 +37,14 @@ public:
 		VOS_SCALE_1,
 	}vos_t;
 	
-	// Voltage scaling 값을 변경합니다.
-	// 이 설정은 MCU의 최대 동작 클럭 주파수에 영향을 미칩니다.
-	//
-	// 반환
-	//		에러를 반환합니다.
-	// vos_t vos
-	//		Voltage Scaling의 값을 설정합니다.
+	/*
+		Voltage scaling 값을 변경합니다.
+		이 설정은 MCU의 최대 동작 클럭 주파수에 영향을 미칩니다.
+		.
+		@ return : 에러를 반환합니다.
+		.
+		@ vos : Voltage Scaling의 값을 설정합니다.
+	*/
 	error_t setVoltageScaling(vos_t vos) __attribute__((optimize("-O1")));
 
 	typedef enum
@@ -49,41 +53,29 @@ public:
 		PLL_SRC_CSI,
 		PLL_SRC_HSE,
 	}pllsrc_t;
-	
-	// 전체 PLL의 소스 클럭을 설정합니다.
-	// PLL1, PLL2, PLL3을 설정하기 전에 반드시 올바른 클럭 소스를 설정해야 합니다.
-	//
-	// 반환
-	//		에러를 반환합니다.
-	// pllsrc_t pllsrc
-	//		PLL 입력 클럭의 소스를 설정합니다.
-	error_t setPllSource(pllsrc_t pllsrc);
 
-	// PLL1을 활성화 합니다. 
-	// PLL2, PLL3을 활성화 하기 전에 반드시 PLL1이 먼저 활성화 되어야 합니다.
-	// PLL의 VCOSEL 설정은 내부에서 자동으로 설정 됩니다.
-	//
-	// 반환
-	//		에러를 반환합니다.
-	// uint8_t m
-	//		PLL 입력 클럭의 분주비를 설정합니다. 
-	//		STM32H723xx에서 설정 가능한 범위는 0 ~ 63입니다. 
-	//		만약 0일 경우 prescaler가 비활성화 되고, 그 외의 경우는 clk / m 이 됩니다.
-	// uint16_t n
-	//		PLL 클럭의 증폭 배율을 설정합니다. 
-	//		STM32H723xx에서 설정 가능한 범위는 3 ~ 511입니다.
-	// uint8_t pdiv
-	//		PLL_P 클럭의 출력 분주비를 설정합니다. 
-	//		STM32H723xx에서 설정 가능한 범위는 0 ~ 127입니다.
-	//		PLL_P의 출력 주파수 계산 : pll_p = f_src / m * (n + 1) / (pDiv + 1)
-	// uint8_t qDiv
-	//		PLL_Q 클럭의 출력 분주비를 설정합니다.
-	//		STM32H723xx에서 설정 가능한 범위는 0 ~ 127입니다.
-	//		PLL_Q의 출력 주파수 계산 : pll_q = f_src / m * (n + 1) / (qDiv + 1)
-	// uint8_t rDiv
-	//		PLL_R 클럭의 출력 분주비를 설정합니다. 설정 가능한 항목은 "define::clock::pll::rdiv"를 참고하세요. 
-	//		STM32H723xx에서 설정 가능한 범위는 0 ~ 127입니다.
-	//		PLL_R의 출력 주파수 계산 : pll_r = f_src / m * (n + 1) / (rDiv + 1)
+	/*	
+		전체 PLL의 소스 클럭을 설정합니다.
+		PLL1, PLL2, PLL3을 설정하기 전에 반드시 올바른 클럭 소스를 설정해야 합니다.
+		.
+		@ return : 에러를 반환합니다.
+		.
+		@ pllsrc : PLL 입력 클럭의 소스를 설정합니다.
+	*/
+	error_t setPllSource(pllsrc_t pllsrc);
+	
+	/*
+		PLL1을 활성화 합니다. 
+		PLL의 VCOSEL 설정은 내부에서 자동으로 설정 됩니다.
+		.
+		@ return : 에러를 반환합니다.
+		.
+		@ m : PLL 입력 클럭의 분주비를 설정합니다. 설정 가능한 범위는 0 ~ 63입니다. 만약 0일 경우 prescaler가 비활성화 되고, 그 외의 경우는 clk / m 이 됩니다.
+		@ n : PLL 클럭의 증폭 배율을 설정합니다. 설정 가능한 범위는 3 ~ 511입니다. 
+		@ pDiv (0 ~ 127) : PLL_P 클럭의 출력 분주비를 설정합니다. pll_p = f_src / m * (n + 1) / (pDiv + 1)
+		@ qDiv (0 ~ 127) : PLL_Q 클럭의 출력 분주비를 설정합니다. pll_q = f_src / m * (n + 1) / (qDiv + 1)
+		@ rDiv (0 ~ 127 / !2) : PLL_R 클럭의 출력 분주비를 설정합니다. 2는 사용불가능 합니다. pll_r = f_src / m * (n + 1) / (rDiv + 1)
+	*/
 	error_t enablePll1(uint8_t m, uint16_t n, uint8_t pDiv, uint8_t qDiv, uint8_t rDiv) __attribute__((optimize("-O1")));
 
 private:
