b813b80e05c309bbc481156a5f9900b3168b1967
Yoon-Ki Hong <mymy49@nate.com>
2024-11-24 17:24:11 +0900

    [STM32F4 ADC 주석 업데이트]

--- inc/drv/Adc.h
+++ inc/drv/Adc.h
@@ -32,35 +32,75 @@ typedef volatile uint32_t	YSS_ADC_Dev;
 #include <targets/common/define_adc_common.h>
 #include <yss/error.h>
 
+/*
+	MCU 내장 ADC 장치의 드라이버 입니다.
+	하나의 ADC로 시분할 하여 각 채널을 ADC 합니다.
+	입력 채널의 해상도와 Low Pass Filter 레벨 설정이 가능합니다.
+	Low Pass Filter 레벨은 정량적이지 않습니다. MCU 실행 성능에 큰 영향을 미치지 않도록 하기 위한 조치입니다.
+	상대적으로 동작 상태를 보고 적당한 레벨로 선택해서 사용합니다.
+	Low Pass Filter가 적용되어 있을 경우 물리적인 ADC 해상도 보다 높은 해상도를 근사치로 만들어 냅니다.
+*/
 class Adc : public Drv
 {
-  public :
-	// ADC 장치를 초기화 한다. 초기화만 했을 뿐, 장치는 정상적인 활성화 되어 있지 않다.
-	// 
-	// 반환
-	//		에러를 반환한다.
+public :
+	/*
+		ADC 장치를 초기화 합니다. 초기화만 했을 뿐, 장치는 정상적인 활성화가 되어 있지 않습니다.
+		.
+		@ return : 발생한 에러를 반환합니다.
+	*/
 	error_t initialize(void);
+	
+	typedef enum
+	{
+		LPF_LV0 = 0,
+		LPF_LV1,
+		LPF_LV2,
+		LPF_LV3,
+		LPF_LV4,
+		LPF_LV5,
+		LPF_LV6,
+		LPF_LV7,
+		LPF_LV8,
+		LPF_LV9,
+		LPF_LV10,
+		LPF_LV11,
+		LPF_LV12,
+		LPF_LV13,
+		LPF_LV14,
+		LPF_LV15,
+		LPF_LV16,
+		LPF_LV17,
+		LPF_LV18,
+		LPF_LV19,
+		LPF_LV20
+	}lpfLv_t;
 
-	// ADC 입력 채널을 추가한다. 단일 ADC 입력 채널을 추가한다.
-	// 입력 채널의 해상도와 Low Pass Filter 레벨 설정이 가능하다.
-	// 
-	// uint8_t pin
-	//		ADC 입력 핀의 고유번호를 설정한다.
-	// uint8_t lpfLv
-	//		Low Pass Filter 레벨을 설정한다. (define::adc::lpfLv::LV0 ~ LV20)
-	// uint8_t bit
-	//		ADC의 해상도를 설정한다. (define::adc::bit::BIT12 ~ BIT16)
-	void add(uint8_t pin, uint8_t lpfLv = define::adc::lpfLv::LV0, uint8_t bit = define::adc::bit::BIT12);
+	typedef enum
+	{
+		BIT12 = 19,
+		BIT13 = 18,
+		BIT14 = 17,
+		BIT15 = 16,
+		BIT16 = 15,
+	}bit_t;
 
-	// ADC 결과 값을 얻어온다.
-	// ADC는 설정된 샘플 시간과 Low Pass Filter 레벨에 의해 계속적으로 업데이트되며
-	// 이 함수를 호출하는 시점의 마지막 ADC 결과 값이 반환된다.
-	//
-	// 반환
-	//		설정된 pin의 ADC 결과값을 반환한다.
-	// uint8_t pin
-	//		결과값을 가져올 ADC Pin을 설정한다.
-	uint16_t get(uint8_t pin);
+	/*
+		ADC 입력 채널을 추가합니다. 한번에 하나의 입력 채널이 추가됩니다.
+		.
+		@ chaanel : ADC 입력 채널의 번호를 설정합니다.
+		@ lpfLv : Low Pass Filter 레벨을 설정합니다. LPF_LV0은 Low Pass Filter가 적용되지 않은 상태입니다.
+		@ bit : ADC의 해상도를 설정합니다.
+	*/
+	void add(uint8_t chaanel, lpfLv_t lpfLv = LPF_LV0, bit_t bit = BIT12);
+	
+	/*
+		설정된 channel의 ADC 결과 값을 반환합니다.
+		.
+		@ result : ADC 결과 값을 반환합니다.
+		.
+		@ channel : 결과값을 가져올 ADC의 채널을 설정합니다.
+	*/
+	uint16_t getResult(uint8_t channel);
 
 #if defined(STM32F0)
 	// 샘플 시간을 설정한다.
@@ -69,16 +109,17 @@ class Adc : public Drv
 	//		샘플 시간을 설정한다. 설정 값은 MCU의 개별 설정에 따라 각기 다르다.
 	void setSampleTime(uint8_t sampleTime);
 #elif defined(STM32F1) || defined(STM32F4) || defined(STM32F7) || defined(GD32F1)
-	// 샘플 시간을 설정한다.
-	// 
-	// uint8_t pin
-	//		샘플 시간을 설정할 ADC Pin을 설정한다. 
-	// uint8_t sampleTime
-	//		샘플 시간을 설정한다. 설정 값은 MCU의 개별 설정에 따라 각기 다르다.
-	void setSampleTime(uint8_t pin, uint8_t sampleTime);
+	/*
+		샘플링 시간을 설정합니다.
+		시분할 하여 ADC 하기 때문에 하나의 채널의 샘플링 시간을 조절하는 것은 전체 샘플링 주기에 영향을 줍니다.
+		.
+		@ channel : 샘플 시간을 설정할 ADC Channel을 설정합니다. 
+		@ sampleTime : 샘플 시간을 설정합니다. 설정 값은 MCU의 개별 설정에 따라 각기 다릅니다.
+	*/
+	void setSampleTime(uint8_t channel, uint8_t sampleTime);
 #endif
 
-	// 아래 함수들은 시스템 함수로 사용자 호출을 금한다.
+	// 아래 함수들은 시스템 함수로 사용자 호출을 권장하지 않습니다.
 	struct setup_t
 	{
 		YSS_ADC_Dev *dev;
@@ -101,6 +142,7 @@ private :
 };
 
 #endif
+
 // ##### 초기화 방법 #####
 //		- GPIO의 setAsAnalog()함수를 사용해 관련된 포트를 아날로그 포트로 변경한다.
 //		- enableClock() 함수를 사용해 장치가 동작할 수 있도록 클럭을 공급한다.
