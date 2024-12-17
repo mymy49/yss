b85a1b26722be572b3f753238f7fe90a04fc9740
Yoon-Ki Hong <mymy49@nate.com>
2024-12-17 19:11:57 +0900

    [Nuvoton M4xx GPIO 업데이트] - GPIO 인터럽트 기능을 추가함

--- src/targets/nuvoton/instance/instance_gpio_nuvoton.cpp
+++ src/targets/nuvoton/instance/instance_gpio_nuvoton.cpp
@@ -20,97 +20,209 @@ const static Drv::setup_t gDrvSetupGpio =
 
 
 #if defined(PA)
+static void enableGpioAInterrupt(bool en)
+{
+	// enableInterrupt() 함수 내부에서 인터럽트를 끄기 때문에 Mutex lock(), unlock()을 하지 않음.
+	nvic.enableInterrupt(GPA_IRQn, en);
+}
+
+const static Drv::setup_t gDrvSetupGpioA =
+{
+	0,						// void (*clockFunc)(bool en);
+	enableGpioAInterrupt,	// void (*nvicFunc)(bool en);
+	0,						// void (*resetFunc)(void);
+	0						// uint32_t (*getClockFunc)(void);
+};
+
 const static Gpio::setup_t gConfigGpioA =
 {
 	PA,				// YSS_GPIO_Peri *dev;
 	&SYS->GPA_MFPL	// volatile uint32_t *mfp;
 };
 
-Gpio gpioA(gDrvSetupGpio, gConfigGpioA);
+Gpio gpioA(gDrvSetupGpioA, gConfigGpioA);
 #endif
 
 
 
 #if defined(PB)
+static void enableGpioBInterrupt(bool en)
+{
+	// enableInterrupt() 함수 내부에서 인터럽트를 끄기 때문에 Mutex lock(), unlock()을 하지 않음.
+	nvic.enableInterrupt(GPB_IRQn, en);
+}
+
+const static Drv::setup_t gDrvSetupGpioB =
+{
+	0,						// void (*clockFunc)(bool en);
+	enableGpioBInterrupt,	// void (*nvicFunc)(bool en);
+	0,						// void (*resetFunc)(void);
+	0						// uint32_t (*getClockFunc)(void);
+};
+
 const static Gpio::setup_t gConfigGpioB =
 {
 	PB,				// YSS_GPIO_Peri *dev;
 	&SYS->GPB_MFPL	// volatile uint32_t *mfp;
 };
 
-Gpio gpioB(gDrvSetupGpio, gConfigGpioB);
+Gpio gpioB(gDrvSetupGpioB, gConfigGpioB);
 #endif
 
 
 
 #if defined(PC)
+static void enableGpioCInterrupt(bool en)
+{
+	// enableInterrupt() 함수 내부에서 인터럽트를 끄기 때문에 Mutex lock(), unlock()을 하지 않음.
+	nvic.enableInterrupt(GPC_IRQn, en);
+}
+
+const static Drv::setup_t gDrvSetupGpioC =
+{
+	0,						// void (*clockFunc)(bool en);
+	enableGpioCInterrupt,	// void (*nvicFunc)(bool en);
+	0,						// void (*resetFunc)(void);
+	0						// uint32_t (*getClockFunc)(void);
+};
+
 const static Gpio::setup_t gConfigGpioC =
 {
 	PC,				// YSS_GPIO_Peri *dev;
 	&SYS->GPC_MFPL	// volatile uint32_t *mfp;
 };
 
-Gpio gpioC(gDrvSetupGpio, gConfigGpioC);
+Gpio gpioC(gDrvSetupGpioC, gConfigGpioC);
 #endif
 
 
 
 #if defined(PD)
+static void enableGpioDInterrupt(bool en)
+{
+	// enableInterrupt() 함수 내부에서 인터럽트를 끄기 때문에 Mutex lock(), unlock()을 하지 않음.
+	nvic.enableInterrupt(GPD_IRQn, en);
+}
+
+const static Drv::setup_t gDrvSetupGpioD =
+{
+	0,						// void (*clockFunc)(bool en);
+	enableGpioDInterrupt,	// void (*nvicFunc)(bool en);
+	0,						// void (*resetFunc)(void);
+	0						// uint32_t (*getClockFunc)(void);
+};
+
 const static Gpio::setup_t gConfigGpioD =
 {
 	PD,				// YSS_GPIO_Peri *dev;
 	&SYS->GPD_MFPL	// volatile uint32_t *mfp;
 };
 
-Gpio gpioD(gDrvSetupGpio, gConfigGpioD);
+Gpio gpioD(gDrvSetupGpioD, gConfigGpioD);
 #endif
 
 
 
 #if defined(PE)
+static void enableGpioEInterrupt(bool en)
+{
+	// enableInterrupt() 함수 내부에서 인터럽트를 끄기 때문에 Mutex lock(), unlock()을 하지 않음.
+	nvic.enableInterrupt(GPE_IRQn, en);
+}
+
+const static Drv::setup_t gDrvSetupGpioE =
+{
+	0,						// void (*clockFunc)(bool en);
+	enableGpioEInterrupt,	// void (*nvicFunc)(bool en);
+	0,						// void (*resetFunc)(void);
+	0						// uint32_t (*getClockFunc)(void);
+};
+
 const static Gpio::setup_t gConfigGpioE =
 {
 	PE,				// YSS_GPIO_Peri *dev;
 	&SYS->GPE_MFPL	// volatile uint32_t *mfp;
 };
 
-Gpio gpioE(gDrvSetupGpio, gConfigGpioE);
+Gpio gpioE(gDrvSetupGpioE, gConfigGpioE);
 #endif
 
 
 
 #if defined(PF)
+static void enableGpioFInterrupt(bool en)
+{
+	// enableInterrupt() 함수 내부에서 인터럽트를 끄기 때문에 Mutex lock(), unlock()을 하지 않음.
+	nvic.enableInterrupt(GPF_IRQn, en);
+}
+
+const static Drv::setup_t gDrvSetupGpioF =
+{
+	0,						// void (*clockFunc)(bool en);
+	enableGpioEInterrupt,	// void (*nvicFunc)(bool en);
+	0,						// void (*resetFunc)(void);
+	0						// uint32_t (*getClockFunc)(void);
+};
+
 const static Gpio::setup_t gConfigGpioF =
 {
 	PF,				// YSS_GPIO_Peri *dev;
 	&SYS->GPF_MFPL	// volatile uint32_t *mfp;
 };
 
-Gpio gpioF(gDrvSetupGpio, gConfigGpioF);
+Gpio gpioF(gDrvSetupGpioF, gConfigGpioF);
 #endif
 
 
 
 #if defined(PG)
+static void enableGpioGInterrupt(bool en)
+{
+	// enableInterrupt() 함수 내부에서 인터럽트를 끄기 때문에 Mutex lock(), unlock()을 하지 않음.
+	nvic.enableInterrupt(GPG_IRQn, en);
+}
+
+const static Drv::setup_t gDrvSetupGpioG =
+{
+	0,						// void (*clockFunc)(bool en);
+	enableGpioGInterrupt,	// void (*nvicFunc)(bool en);
+	0,						// void (*resetFunc)(void);
+	0						// uint32_t (*getClockFunc)(void);
+};
+
 const static Gpio::setup_t gConfigGpioG =
 {
 	PG,				// YSS_GPIO_Peri *dev;
 	&SYS->GPG_MFPL	// volatile uint32_t *mfp;
 };
 
-Gpio gpioG(gDrvSetupGpio, gConfigGpioG);
+Gpio gpioG(gDrvSetupGpioG, gConfigGpioG);
 #endif
 
 
 
 #if defined(PH)
+static void enableGpioHInterrupt(bool en)
+{
+	// enableInterrupt() 함수 내부에서 인터럽트를 끄기 때문에 Mutex lock(), unlock()을 하지 않음.
+	nvic.enableInterrupt(GPH_IRQn, en);
+}
+
+const static Drv::setup_t gDrvSetupGpioH =
+{
+	0,						// void (*clockFunc)(bool en);
+	enableGpioHInterrupt,	// void (*nvicFunc)(bool en);
+	0,						// void (*resetFunc)(void);
+	0						// uint32_t (*getClockFunc)(void);
+};
+
 const static Gpio::setup_t gConfigGpioH =
 {
 	PH,				//YSS_GPIO_Peri *dev;
 	&SYS->GPH_MFPL	// volatile uint32_t *mfp;
 };
 
-Gpio gpioH(gDrvSetupGpio, gConfigGpioH);
+Gpio gpioH(gDrvSetupGpioH, gConfigGpioH);
 #endif
 
 #endif
