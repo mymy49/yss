4611674547091d0327b294bd51da0288b2fe25fb
Yoon-Ki Hong <mymy49@nate.com>
2025-01-01 15:26:09 +0900

    [Nuvoton M4xx CLK 장치 주석 업데이트] - 주석 폼을 최근 방식으로 변경

--- inc/targets/nuvoton/class_clock.h
+++ inc/targets/nuvoton/class_clock.h
@@ -16,57 +16,62 @@
 class Clock : public Mutex
 {
 public:
-	// External High Speed Crystal Oscilator를 활성화 합니다.
-	//
-	// 반환
-	//		에러를 반환합니다.
-	// uint32_t hseHz
-	//		외부 크리스털의 주파수를 HZ 단위로 입력합니다.
-	// useBypass = false
-	//		입력이 크리스탈일 경우에는 false로 설정합니다.
-	//		입력이 오실레이터일 경우나 클럭 소스를 직접 입력 받을 경우 bypass를 true로 설정합니다.
+	/*
+		External High Speed Crystal Oscilator를 활성화 합니다.
+		.
+		@ return : 에러를 반환합니다.
+		.
+		@ hseHz : 외부 크리스털의 주파수를 HZ 단위로 입력합니다.
+	*/
 	error_t enableHxt(uint32_t hseHz) __attribute__((optimize("-O1")));
 	
-	// High Internal speed Osilator의 주파수를 얻습니다.
-	// 
-	// 반환
-	//		Hirc 오실레이터의 주파수를 반환 합니다.
+	/*	
+		High Internal speed Osilator의 주파수를 얻습니다.
+		.
+		@ return : Hirc 오실레이터의 주파수를 반환 합니다.
+	*/
 	uint32_t getHircFrequency(void) __attribute__((optimize("-O1")));
-
-	// External High Speed Crystal Oscilator의 주파수를 얻습니다.
-	// 
-	// 반환
-	//		외부 오실레이터의 주파수를 반환 합니다.
+	
+	/*
+		External High Speed Crystal Oscilator의 주파수를 얻습니다.
+		.
+		@ return : 외부 오실레이터의 주파수를 반환 합니다.
+	*/
 	uint32_t getHxtFrequency(void) __attribute__((optimize("-O1")));
-
-	// Internal High speed Osilator의 주파수를 얻습니다.
-	// 
-	// 반환
-	//		Hirc 오실레이터의 주파수를 반환 합니다.
+	
+	/*
+		Internal High speed Osilator의 주파수를 얻습니다.
+		.
+		@ return : Hirc 오실레이터의 주파수를 반환 합니다.
+	*/
 	uint32_t getLircFrequency(void) __attribute__((optimize("-O1")));
 
-	// PLL의 주파수를 얻습니다.
-	// 
-	// 반환
-	//		PLL의 주파수를 반환 합니다.
+	/*
+		PLL의 주파수를 얻습니다.
+		.
+		@ return : PLL의 주파수를 반환 합니다.
+	*/
 	uint32_t getPllFrequency(void) __attribute__((optimize("-O1")));
-
-	// HCLK 클럭의 주파수를 얻습니다.
-	//
-	// 반환
-	//		HCLK 클럭의 주파수를 반환합니다.
+	
+	/*
+		HCLK 클럭의 주파수를 얻습니다.
+		.
+		@ return : HCLK 클럭의 주파수를 반환합니다.
+	*/
 	uint32_t getHclkClockFrequency(void) __attribute__((optimize("-O1")));
-
-	// APB0 클럭의 주파수를 얻습니다.
-	//
-	// 반환
-	//		APB0 클럭의 주파수를 반환합니다.
+	
+	/*
+		APB0 클럭의 주파수를 얻습니다.
+		.
+		@ return : APB0 클럭의 주파수를 반환합니다.
+	*/
 	uint32_t getApb0ClockFrequency(void) __attribute__((optimize("-O1")));
-
-	// APB1 클럭의 주파수를 얻습니다.
-	//
-	// 반환
-	//		APB1 클럭의 주파수를 반환합니다.
+	
+	/*
+		APB1 클럭의 주파수를 얻습니다.
+		.
+		@ return : APB1 클럭의 주파수를 반환합니다.
+	*/
 	uint32_t getApb1ClockFrequency(void) __attribute__((optimize("-O1")));
 
 	// PLL 클럭 소스
@@ -76,21 +81,20 @@ public:
 		PLL_SRC_HIRC,
 	}pllSrc_t;
 	
-	// PLL을 활성화 합니다. 출력 주파수는 아래의 식과 같습니다.
-	// FOUT = FIN x (2 x (fbdiv + 2)) / NO
-	// 위 식에서 NO는 아래와 같이 outdiv의 값에 의해 결정 됩니다.
-	// outdiv == 0, NO = 1
-	// outdiv == 1 또는 outdiv == 2, NO = 2
-	// outdiv == 3, NO = 4
-	//
-	// 반환
-	//		에러를 반환합니다.
-	// uint8_t indiv
-	//		입력 클럭 분주기입니다. (0 ~ 63)
-	// uint16_t fbdiv
-	//		PLL 배수기입니다. (0 ~ 512)
-	// uint8_t outdiv
-	//		출력 클럭 분주기 입니다. (0~3)
+	/*
+		PLL을 활성화 합니다. 출력 주파수는 아래의 식과 같습니다.
+		FOUT = FIN x (2 x (fbdiv + 2)) / NO;
+		위 식에서 NO는 아래와 같이 outdiv의 값에 의해 결정 됩니다.
+		outdiv == 0, NO = 1;
+		outdiv == 1 또는 outdiv == 2, NO = 2;
+		outdiv == 3, NO = 4;
+		.
+		@ return : 에러를 반환합니다.
+		.
+		@ indiv (0 ~ 63) : 입력 클럭 분주기입니다.
+		@ fbdiv (0 ~ 512) : PLL 배수기입니다. 
+		@ outdiv (0 ~ 3) : 출력 클럭 분주기 입니다. 
+	*/
 	error_t enablePll(pllSrc_t src, uint8_t indiv, uint16_t fbdiv, uint8_t outdiv) __attribute__((optimize("-O1")));
 
 	// HCLK 클럭 소스
@@ -102,42 +106,44 @@ public:
 		HCLK_SRC_LRIC,
 		HCLK_SRC_HIRC,
 	}hclkSrc_t;
-
-	// HCLK의 클럭 소스를 선택합니다. 이 함수 내부에서 Flash의 Access Cycle 설정이 자동으로 이뤄집니다. 
-	//
-	// 반환
-	//		에러를 반환합니다.
-	// hclkSrc_t src
-	//		HCLK의 클럭럭 소스를 설정합니다.
-	// uint8_t hclkDiv
-	//		HCLK의 분주비를 설정합니다. PLL이 소스일 경우 반드시 0으로 설정해야 합니다. 
-	// uint8_t pclk0Div
-	//		PCLK0의 분주비를 설정합니다. HCLK의 주파수로 부터 분주됩니다. 
-	//		M483에서 주파수가 96MHz를 넘으면 안됩니다.
-	// uint8_t pclk1Div
-	//		PCLK1의 분주비를 설정합니다. HCLK의 주파수로 부터 분주됩니다.
-	//		M483에서 주파수가 96MHz를 넘으면 안됩니다.
+	
+	/*
+		HCLK의 클럭 소스를 선택합니다. 
+		이 함수 내부에서 Flash의 Access Cycle 설정이 자동으로 이뤄집니다. 
+		.
+		@ return : 에러를 반환합니다.
+		.
+		@ src : HCLK의 클럭럭 소스를 설정합니다.
+		@ hclkDiv : HCLK의 분주비를 설정합니다. PLL이 소스일 경우 반드시 0으로 설정해야 합니다. 
+		@ pclk0Div : PCLK0의 분주비를 설정합니다. HCLK의 주파수로 부터 분주됩니다.
+		@ pclk1Div : PCLK1의 분주비를 설정합니다. HCLK의 주파수로 부터 분주됩니다.
+		.
+		M483 : PCLK0, PCLK1의 주파수가 96MHz를 넘으면 안됩니다.
+	*/
 	error_t setHclkClockSource(hclkSrc_t src, uint8_t hclkDiv, uint8_t pclk0Div, uint8_t pclk1Div) __attribute__((optimize("-O1")));
-
-	// AHB 버스 장치의 클럭을 활성화 합니다.
-	// uint32_t position
-	//		AHB 버스의 활성화할 비트필드의 비트 번호를 설정합니다.
-	// bool en = true
-	//		AHB 버스의 position에서 설정한 장치를 켜거(true)나 끕(false)니다.
+	
+	/*
+		AHB 버스 장치의 클럭을 활성화/비활성화 합니다.
+		.
+		@ position : AHB 버스의 활성화할 비트필드의 비트 번호를 설정합니다.
+		@ en : AHB 버스의 position에서 설정한 장치를 켜거(true)나 끕(false)니다.
+	*/
 	void enableAhbClock(uint32_t position, bool en = true) __attribute__((optimize("-O1")));
-
-	// APB0 버스 장치의 클럭을 활성화 합니다.
-	// uint32_t position
-	//		APB0 버스의 활성화할 비트필드의 비트 번호를 설정합니다.
-	// bool en = true
-	//		APB0 버스의 position에서 설정한 장치를 켜거(true)나 끕(false)니다.
+	
+	/*
+		APB0 버스 장치의 클럭을 활성화/비활성화 합니다.
+		.
+		@ position : APB0 버스의 활성화할 비트필드의 비트 번호를 설정합니다.
+		@ en : APB0 버스의 position에서 설정한 장치를 켜거(true)나 끕(false)니다.
+	*/
 	void enableApb0Clock(uint32_t position, bool en = true) __attribute__((optimize("-O1")));
 
-	// APB1 버스 장치의 클럭을 활성화 합니다.
-	// uint32_t position
-	//		APB1 버스의 활성화할 비트필드의 비트 번호를 설정합니다.
-	// bool en = true
-	//		APB1 버스의 position에서 설정한 장치를 켜거(true)나 끕(false)니다.
+	/*
+		APB1 버스 장치의 클럭을 활성화/비활성화 합니다.
+		.
+		@ position : APB1 버스의 활성화할 비트필드의 비트 번호를 설정합니다.
+		@ en : APB1 버스의 position에서 설정한 장치를 켜거(true)나 끕(false)니다.
+	*/
 	void enableApb1Clock(uint32_t position, bool en = true) __attribute__((optimize("-O1")));
 
 private:
