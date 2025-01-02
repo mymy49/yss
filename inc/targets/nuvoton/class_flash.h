513fd2451f81089fb2feb793d7fb7664fc42821d
Gillian <Gillian@fanlight.com>
2025-01-02 10:04:22 +0900

    [Nuvoton M4xx FLASH 코드 업데이트]  - 내부 플래시 4바이트 읽기 추가  - FMC-ISPCMD 읽기 값 오류 수정

--- inc/targets/nuvoton/class_flash.h
+++ inc/targets/nuvoton/class_flash.h
@@ -22,12 +22,16 @@ public :
 
 	error_t program(uint16_t page, uint32_t *src, uint32_t count) __attribute__((optimize("-O1")));
 	
+	error_t read4Xbytes(uint16_t page, uint16_t sector, uint16_t count, uint32_t *dataReg);				//page : 페이지 , sector : 페이지 내 위치(4byte 단위), count : 읽어올 바이트 수 (1 입력 시 4바이트 리드), dataReg : 읽은 데이터 기록할 주소 
+	
 private :
 	error_t executeCommand(uint8_t cmd) __attribute__((optimize("-O1")));
 
 	error_t enable(bool en) __attribute__((optimize("-O1")));
 
 	error_t program32bit(uint32_t addr, uint32_t data) __attribute__((optimize("-O1")));
+
+	error_t read32bit(uint32_t addr, uint32_t* data) __attribute__((optimize("-O1")));
 };
 
 #endif
